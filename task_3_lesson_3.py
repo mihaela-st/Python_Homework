f403938afcf47abaf1b485030bc9d93294e8e4e3
mihaela-st <mihaela.stepanchevici@gmail.com>
2024-04-13 23:18:47 +0300

    13.04.2024

---
+++ task_3_lesson_3.py
@@ -0,0 +1,95 @@
+# Aceasta este a treia ta sarcină legată de numere în python
+# Creează o variabilă numită `number` și atribuie-i valoarea `10`
+
+# CODUL TĂU VINE MAI JOS:
+number=10
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează valoarea variabilei `number` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(number)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `number2` și atribuie-i valoarea `number` și afișează valoarea variabilei `number2` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+number2=number
+print(number2)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `number3` și atribuie-i valoarea `4`
+
+# CODUL TĂU VINE MAI JOS:
+number3=4
+# CODUL TĂU VINE MAI SUS:
+
+# Acum aplică toate operațiile pe care le-ai învățat în lecție asupra variabilelor `number` și `number3` și afișează rezultatele folosind funcția `print`
+# Operațiile sunt: adunare, scădere, înmulțire, împărțire, modulo, exponențiere, împărțire întreagă
+
+# CODUL TĂU VINE MAI JOS:
+adunare=number+number3
+print(adunare)
+scadere=number-number3
+print(scadere)
+inmultire=number*number3
+print(inmultire)
+impartire=number/number3
+print(impartire)
+modulo=number%number3
+print(modulo)
+exponentiere=number**number3
+print(exponentiere)
+impartire_intreaga=number//number3
+print(impartire_intreaga)
+# CODUL TĂU VINE MAI SUS:
+
+# Creează o nouă variabilă numită `number4` și atribuie-i o valoare numerică mare într-un mod literal
+
+# CODUL TĂU VINE MAI JOS:
+number4=1e7
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează tipul variabilei `number4` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(number4)
+# CODUL TĂU VINE MAI SUS:
+
+# Creați o variabilă `numar` și atribuiții valoarea 5.0
+
+# CODUL TĂU VINE MAI JOS:
+numar=5.0
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișați tipul lui `numar` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(numar)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creați o variabilă `numar2` și atribuiții valoarea 2.0
+
+# CODUL TĂU VINE MAI JOS:
+numar2=2.0
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creați o variabila `numar_mare` și atribuiți-i o valoare mare utilizând numerele literale
+
+# CODUL TĂU VINE MAI JOS:
+numar_mare=1e9
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișați tipul lui `numar_mare` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(numar_mare)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum ștergeți variabila `numar_mare` folosind instrucțiunea `del`
+
+# CODUL TĂU VINE MAI JOS:
+del(numar_mare)
+# CODUL TĂU VINE MAI SUS:
+
+# Asta a fost tot pentru această sarcină
